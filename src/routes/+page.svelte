c2819022f452bd4c17d280ed4a4f024c262338ae
Liam <75332524+liambsulliva@users.noreply.github.com>
2025-01-28 18:21:03 -0500

    fix spacing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,11 @@
 	import TreeGraph from '$lib/TreeGraph.svelte';
 </script>
 
-<section class="m-4">
-	<h1 class="mb-2 text-3xl font-bold">Is Produce Really All That Expensive?</h1>
-	<p>There has been a lot of chatter on grocery prices as of late.</p>
+<section class="m-6">
+	<div class="my-4">
+		<h1 class="mb-1 text-3xl font-bold">Is Produce Really All That Expensive?</h1>
+		<p>There has been a lot of chatter on grocery prices as of late.</p>
+	</div>
+
+	<TreeGraph />
 </section>
-<TreeGraph />
