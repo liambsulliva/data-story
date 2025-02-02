6be7c904dd44804d45921d160f28abcece1e3591
Liam <75332524+liambsulliva@users.noreply.github.com>
2025-02-01 19:06:25 -0500

    use only fruit assets

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,25 +3,26 @@
 	import InteractiveCollage from '$lib/InteractiveCollage.svelte';
 	import Link from '$lib/Link.svelte';
 	import Apple from '/src/assets/Apple.svg?raw';
-	import Broccoli from '/src/assets/Broccoli.svg?raw';
-	import Pepper from '/src/assets/Pepper.svg?raw';
-	import Orange from '/src/assets/Orange.svg?raw';
-	import Lemons from '/src/assets/Lemons.svg?raw';
 	import Grapes from '/src/assets/Grapes.svg?raw';
+	import Pomegranate from '/src/assets/Pomegranate.svg?raw';
+	import Peach from '/src/assets/Peach.svg?raw';
+	import Lemons from '/src/assets/Lemons.svg?raw';
+	import Plum from '/src/assets/Plum.svg?raw';
+	import Pineapple from '/src/assets/Pineapple.svg?raw';
 	import EmphasisScribble from '/src/assets/emphasis-scribble.svg?raw';
 </script>
 
 <main class="mx-auto w-full max-w-[1200px]">
 	<div class="flex justify-center">
 		<div class="relative inline-flex">
-			<h1 class="m-24 text-center font-freightText text-5xl font-bold">
+			<h1 class="m-24 text-center font-freightText text-5xl font-bold max-md:mx-6">
 				How Expensive Are Fruits, Really?
 			</h1>
 			<div class="absolute bottom-[5rem] left-12 right-12 h-[2px] bg-black"></div>
 			<div class="absolute bottom-[4.75rem] left-12 right-12 h-[2px] bg-black"></div>
 		</div>
 	</div>
-	<section class="sm:mt-16">
+	<section class="sm:mt-16 lg:mx-8">
 		<div class="flex flex-col justify-between gap-8 px-6 max-lg:items-center lg:flex-row">
 			<div>
 				<p class="font-proximaNova py-3">
@@ -44,19 +45,19 @@
 			<div>
 				<div class="relative">
 					<div
-						class="text-md absolute -right-2.5 -top-2.5 z-10 border px-4 py-2 font-freightText text-lg font-bold shadow-[4px_4px_0px_#f0f0f0] max-md:hidden"
+						class="text-md absolute -right-2.5 -top-2.5 z-10 border px-4 py-2 font-freightText text-lg font-bold shadow-[4px_4px_0px_#f0f0f0] max-lg:hidden"
 					>
 						Hover Me!
 						<div class="absolute -right-20 -top-12 w-24 rotate-12">
 							{@html EmphasisScribble}
 						</div>
 					</div>
-					<InteractiveCollage svg1={Broccoli} svg2={Pepper} svg3={Orange} />
+					<InteractiveCollage svg1={Pomegranate} svg2={Peach} svg3={Grapes} />
 				</div>
 			</div>
 		</div>
 	</section>
-	<section class="mt-12">
+	<section class="mt-12 lg:mx-8">
 		<div class="relative mx-6 mb-12 inline-flex items-center">
 			<h2 class="mr-2 text-center font-freightText text-5xl font-bold">Fresh</h2>
 			{@html Apple}
@@ -75,10 +76,10 @@
 			</div>
 		</div>
 	</section>
-	<section class="mt-36">
+	<section class="mt-36 lg:mx-8">
 		<div class="relative mx-6 mb-12 inline-flex items-center">
-			<h2 class="mr-4 text-center font-freightText text-5xl font-bold">Frozen</h2>
-			{@html Grapes}
+			<h2 class="mr-2 text-center font-freightText text-5xl font-bold">Frozen</h2>
+			{@html Plum}
 			<div class="absolute bottom-0 left-0 right-0">
 				<img src="/src/assets/header-scribble.svg" alt="Underline" class="w-36" />
 			</div>
@@ -94,10 +95,10 @@
 			</div>
 		</div>
 	</section>
-	<section class="mt-36">
+	<section class="mt-36 lg:mx-8">
 		<div class="relative mx-6 mb-12 inline-flex items-center">
 			<h2 class="mr-2 text-center font-freightText text-5xl font-bold">Canned</h2>
-			{@html Lemons}
+			{@html Pineapple}
 			<div class="absolute -bottom-1 left-0 right-0">
 				<img src="/src/assets/header-scribble.svg" alt="Underline" class="w-40" />
 			</div>
@@ -113,10 +114,10 @@
 			</div>
 		</div>
 	</section>
-	<section class="mt-36">
+	<section class="mt-36 lg:mx-8">
 		<div class="relative mx-6 mb-12 inline-flex items-center">
-			<h2 class="mr-2 text-center font-freightText text-5xl font-bold">Dried</h2>
-			{@html Lemons}
+			<h2 class="mr-4 text-center font-freightText text-5xl font-bold">Dried</h2>
+			{@html Grapes}
 			<div class="absolute bottom-0 left-0 right-0">
 				<img src="/src/assets/header-scribble.svg" alt="Underline" class="w-32" />
 			</div>
@@ -132,7 +133,7 @@
 			</div>
 		</div>
 	</section>
-	<section class="mt-36">
+	<section class="mt-36 lg:mx-8">
 		<div class="relative mx-6 mb-12 inline-flex items-center">
 			<h2 class="mr-2 text-center font-freightText text-5xl font-bold">Juice</h2>
 			{@html Lemons}
