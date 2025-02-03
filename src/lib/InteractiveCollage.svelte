1dda277c4d3feaf1ad23b0885abc513821ba0723
Liam <75332524+liambsulliva@users.noreply.github.com>
2025-02-02 22:08:32 -0500

    remove z-index stores

--- src/lib/InteractiveCollage.svelte
+++ src/lib/InteractiveCollage.svelte
@@ -1,29 +1,22 @@
 <script lang="ts">
 	import { spring } from 'svelte/motion';
-	import { writable } from 'svelte/store';
 
 	// 3D position for spring()
 	interface Position {
 		x: number;
 		y: number;
-		z: number;
 	}
 
 	export let svg1: string;
 	export let svg2: string;
 	export let svg3: string;
-	// SVG size
 	export let size: number = 300;
-
-	const z1 = writable(1);
-	const z2 = writable(1);
-	const z3 = writable(1);
 
 	// Default positions
 	const defaults = {
-		svg1: { x: -80, y: -60, z: 1 },
-		svg2: { x: 80, y: -60, z: 1 },
-		svg3: { x: 20, y: 60, z: 1 }
+		svg1: { x: -80, y: -60 },
+		svg2: { x: 80, y: -60 },
+		svg3: { x: 20, y: 60 }
 	};
 
 	// Spring stores for each SVG's position
@@ -45,7 +38,6 @@
 		event: MouseEvent,
 		pos: typeof pos1,
 		defaultPos: Position,
-		activeZ: typeof z1
 	) {
 		const container = event.currentTarget as HTMLDivElement;
 		const rect = container.getBoundingClientRect();
@@ -60,15 +52,9 @@
 		const deltaX = (mouseX - centerX) / 10;
 		const deltaY = (mouseY - centerY) / 10;
 
-		// Set other fruits to z-1
-		z1.set(activeZ === z1 ? 3 : 1);
-		z2.set(activeZ === z2 ? 3 : 1);
-		z3.set(activeZ === z3 ? 3 : 1);
-
 		pos.set({
 			x: defaultPos.x + deltaX,
 			y: defaultPos.y + deltaY,
-			z: 3 // This z is just for the spring animation, not used for display
 		});
 	}
 
@@ -82,11 +68,10 @@
 		class="svg-container"
 		style="
                 transform: translate({$pos1.x}px, {$pos1.y}px);
-                z-index: {$z1};
                 width: {size}px;
                 height: {size}px;
             "
-		on:mousemove={(e) => handleMouseMove(e, pos1, defaults.svg1, z1)}
+		on:mousemove={(e) => handleMouseMove(e, pos1, defaults.svg1)}
 		on:mouseleave={() => handleMouseLeave(pos1, defaults.svg1)}
 	>
 		{@html svg1}
@@ -95,11 +80,10 @@
 		class="svg-container"
 		style="
                 transform: translate({$pos2.x}px, {$pos2.y}px);
-                z-index: {$z2};
                 width: {size}px;
                 height: {size}px;
             "
-		on:mousemove={(e) => handleMouseMove(e, pos2, defaults.svg2, z2)}
+		on:mousemove={(e) => handleMouseMove(e, pos2, defaults.svg2)}
 		on:mouseleave={() => handleMouseLeave(pos2, defaults.svg2)}
 	>
 		{@html svg2}
@@ -108,11 +92,10 @@
 		class="svg-container"
 		style="
                 transform: translate({$pos3.x}px, {$pos3.y}px);
-                z-index: {$z3};
                 width: {size}px;
                 height: {size}px;
             "
-		on:mousemove={(e) => handleMouseMove(e, pos3, defaults.svg3, z3)}
+		on:mousemove={(e) => handleMouseMove(e, pos3, defaults.svg3)}
 		on:mouseleave={() => handleMouseLeave(pos3, defaults.svg3)}
 	>
 		{@html svg3}
